9c3bb4e17af89fc4e355afdec909fb689e68717c
Andre Barbe <31875393+andre-barbe@users.noreply.github.com>
2021-11-19 12:34:33 -0500

    Also modified to export results for ANX1

--- EnvisageNOV2021/ANX1/runSim.cmd
+++ EnvisageNOV2021/ANX1/runSim.cmd
@@ -1,3 +1,3 @@
 set baseName=ANX1
-set oDir=z:\Output\Env10\%baseName%
+set oDir=..\%baseName%\Results
 gams runSim --simName=%1 --BauName=%2 --simType=%3 --ifCal=%4 --baseName=%baseName% --odir=%oDir% -idir=..\Model -scrdir=%oDir% -ps=9999 -pw=121 -errmsg=1
